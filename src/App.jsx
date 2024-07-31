061634601c65ea7be15fc3bd368069634543399d
sugimoto.yuki <yuuki4135@gmail.com>
2024-07-31 22:27:29 +0900

    mvp2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,63 @@
 import { useState } from 'react'
 
 function App() {
+  const [records, setRecords] = useState([])
+  const [title, setTitle] = useState('')
+  const [time, setTime] = useState(0)
+  const [error, setError] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (!title || typeof time != 'number') {
+      setError('入力されていない項目があります')
+    }else{
+      setRecords([...records, { title, time }])
+      setTitle('')
+      setTime(0)
+      setError('')
+    }
+  }
 
   return (
     <>
       <h1>学習記録一覧</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            学習内容
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            学習時間
+            <input
+              type="number"
+              value={time}
+              onChange={(e) => setTime(Number(e.target.value))}
+            />
+            時間
+          </label>
+        </div>
+        <div>
+          入力されている学習内容: {title}
+        </div>
+        <div>
+          入力されている学習時間: {time}時間
+        </div>
+        {records.map((record, index) => (
+          <div key={index}>
+            {record.title}: {record.time}時間
+          </div>
+        ))}
+        <button type="submit">追加</button>
+        <div>{error}</div>
+      </form>
     </>
   )
 }
