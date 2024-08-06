b8516de4ea169da70f0e45b37292f754d8fe1be8
sugimoto.yuki <yuuki4135@gmail.com>
2024-08-06 22:58:46 +0900

    mvp1完了

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
 import { createClient } from '@supabase/supabase-js'
+import { Load } from './compornents/organisms/load'
 
-const supabaseUrl = 'https://fvkzpaeinzoxaniwyqqi.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseUrl = process.env.VITE_SUPABASE_URL
+const supabaseKey = process.env.VITE_SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 function App() {
@@ -11,6 +11,7 @@ function App() {
   const [title, setTitle] = useState('')
   const [time, setTime] = useState(0)
   const [error, setError] = useState('')
+  const [loading, setLoading] = useState(true)
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -25,46 +26,63 @@ function App() {
     }
   }
 
+  useEffect(() => {
+    const fetchRecords = async () => {
+      const { data, error } = await supabase
+        .from('study-record')
+        .select('*')
+      if (error) {
+        console.error(error)
+      } else {
+        setRecords(data)
+        setLoading(false)
+      }
+    }
+    fetchRecords()
+  }, [])
+
   return (
     <>
-      <h1>学習記録一覧</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            学習内容
-            <input
-              type="text"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            学習時間
-            <input
-              type="number"
-              value={time}
-              onChange={(e) => setTime(Number(e.target.value))}
-            />
-            時間
-          </label>
-        </div>
-        <div>
-          入力されている学習内容: {title}
-        </div>
-        <div>
-          入力されている学習時間: {time}時間
-        </div>
-        {records.map((record, index) => (
-          <div key={index}>
-            {record.title}: {record.time}時間
+      <Load loading={loading}>
+        <h1>学習記録一覧</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>
+              学習内容
+              <input
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </label>
           </div>
-        ))}
-        <button type="submit">追加</button>
-        <div>{error}</div>
-        <div>合計時間: {records.reduce((acc, record) => acc + record.time, 0)} / 1000 (h)</div>
-      </form>
+          <div>
+            <label>
+              学習時間
+              <input
+                type="number"
+                value={time}
+                onChange={(e) => setTime(Number(e.target.value))}
+              />
+              時間
+            </label>
+          </div>
+          <div>
+            入力されている学習内容: {title}
+          </div>
+          <div>
+            入力されている学習時間: {time}時間
+          </div>
+          {records.map((record, index) => (
+            <div key={index}>
+              {record.title}: {record.time}時間
+            </div>
+          ))}
+          <button type="submit">追加</button>
+          <div>{error}</div>
+          <div>合計時間: {records.reduce((acc, record) => acc + record.time, 0)} / 1000 (h)</div>
+        </form>
+      </Load>
     </>
   )
 }
